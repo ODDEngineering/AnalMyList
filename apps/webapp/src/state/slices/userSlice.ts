be75376597cedc4d72ef4f2dc90a99aab9c3ab0e
SeihakRithy Muth <Rithy58@gmail.com>
2022-02-27 17:40:55 -0500

    Added initial user slice

---
+++ apps/webapp/src/state/slices/userSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface userState {
+  loggedin: boolean,
+  username: string,
+}
+
+const initialUserState: userState = {
+  loggedin: false,
+  username: ''
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: initialUserState,
+  reducers: {
+    login: (state, action: PayloadAction<string>) => {
+      state.loggedin = true
+      state.username = action.payload
+    },
+    logout: (state) => {
+      state.loggedin = false
+      state.username = ''
+    }
+  }
+})
