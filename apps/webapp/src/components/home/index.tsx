cfe67d5413278698b044c4e94d41464c04da4577
Rithy Muth <Rithy58@users.noreply.github.com>
2022-03-14 11:31:36 -0700

    Moved content from App to Home component

---
+++ apps/webapp/src/components/home/index.tsx
@@ -0,0 +1,20 @@
+import { Message } from '@anal-my-list/api-interfaces';
+import React from 'react'
+
+export const Home = (): JSX.Element => {
+  const [m, setMessage] = React.useState<Message>({ message: '' });
+
+  React.useEffect(() => {
+    fetch('/api/')
+      .then((r) => r.json())
+      .then(setMessage);
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Welcome to Anal My List</h1>
+      <h2>An app for analyzing your personality based on your playlist</h2>
+      <div>{m.message}</div>
+    </div>
+  )
+}
