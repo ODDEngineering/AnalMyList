dc452d747ffd9a268274eb64f081417ddb84707a
Rithy Muth <Rithy58@users.noreply.github.com>
2022-03-14 14:02:59 -0700

    Implemented Routes

--- apps/webapp/src/components/app/app.tsx
+++ apps/webapp/src/components/app/app.tsx
@@ -3,12 +3,19 @@ import { Message } from '@anal-my-list/api-interfaces';
 import { Provider } from 'react-redux';
 import { store } from '../../state';
 import { Home } from '../home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { About } from '../about';
 
 export const App = () => {
 
   return (
     <Provider store={store}>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 };
